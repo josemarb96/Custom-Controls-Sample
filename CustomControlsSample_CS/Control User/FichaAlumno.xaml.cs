88b03df0511ca48a5f4e90c718a07b23856a983c
josemarb96 <josemarb96@gmail.com>
2016-11-21 13:06:00 +0100

    Custom Control Sample

---
+++ CustomControlsSample_CS/Control User/FichaAlumno.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CustomControlsSample_CS.Control_User
+{
+    public sealed partial class FichaAlumno : UserControl
+    {
+        public FichaAlumno()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+        public ImageSource Profile
+        {
+            get { return (ImageSource)GetValue(ProfileProperty); }
+            set { SetValue(ProfileProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Profile.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ProfileProperty =
+            DependencyProperty.Register("Profile", typeof(ImageSource), typeof(FichaAlumno), 
+                new PropertyMetadata(new BitmapImage(new Uri("ms-appx://CustomControlsSample_CS/Assets/personita.png"))));
+        //Tu namespace y la foto
+
+
+        public string StudentName
+        {
+            get { return (string)GetValue(StudentNameProperty); }
+            set { SetValue(StudentNameProperty, value); }
+        }
+
+        //propdp + doble tabulación --> autogenera get y set
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StudentNameProperty =
+            DependencyProperty.Register("StudentName", typeof(int), typeof(FichaAlumno), new PropertyMetadata(String.Empty));
+
+        public int Age
+        {
+            get { return (int)GetValue(AgeProperty); }
+            set { SetValue(AgeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Age.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AgeProperty =
+            DependencyProperty.Register("Age", typeof(int), typeof(FichaAlumno), new PropertyMetadata(0));
+    }
+}
